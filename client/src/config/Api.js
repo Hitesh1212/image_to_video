5ed728a0ce811834efde1b913a6faf667a35944e
Hitesh Ranjan <hiteshranjan22@gmail.com>
2025-05-16 16:02:38 +0530

    Update Api.js
    base url changes

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:3005/api/v1";
+//export const BASE_URL = "http://localhost:3005/api/v1";
+//https://backend-image-to-video.onrender.com/api/v1
+export const BASE_URL = "https://backend-image-to-video.onrender.com/api/v1";
